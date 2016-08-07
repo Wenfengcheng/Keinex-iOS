32246cc39502033cbbfbdf4f53643dc18d30c452
Andrey Baranchikov <info@keinex.ru>
2016-08-07 03:05:44 +0300

    Test git

--- Keinex/LatestPostsTableViewController.swift
+++ Keinex/LatestPostsTableViewController.swift
@@ -52,7 +52,6 @@ class LatestNewsTableViewController: UITableViewController {
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     // MARK: - Table view data source
